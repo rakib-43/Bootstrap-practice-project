ad53eac47b85541e0e736e0105d02f8979e05de0
Rakib hasan <hasanrakib739@gmail.com>
2023-06-19 12:32:14 +0600

    On scroll function for navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,32 @@
+//On scroll function 
+function navMenu(){
+    let navBar = document.querySelector('.navbar-sticky');
+    let scrollTopButton = document.querySelector("#scrollUp");
+    window.onscroll = function (){
+        var scroll = document.documentElement.scrollTop;
+        if (scroll >= 120){
+            navBar.classList.add("navbar-sticky-moved-up");
+        } else{
+            navBar.classList.remove("navbar-sticky-moved-up");
+        }
+        //apply transition
+        if(scroll >= 250){
+            navBar.classList.add('navbar-sticky-transitioned');
+        }
+        else {
+            navBar.classList.remove("navbar-sticky-transitioned");
+        }
+        // sticky on
+        if(scroll >=500){
+            navBar.classList.add("navbar-sticky-on");
+        } else{
+            navBar.classList.remove("navbar-sticky-on");
+        }
+    }
+}
+navMenu();
+
+
 // Counter Design
 document.addEventListener("DOMContentLoaded",() =>{
     function counter( id, start, end, duration){
